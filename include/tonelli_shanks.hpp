6b6044557ebaa135ea1e413ea090cf921e8027d4
Leviathan <soaresjsantos93@gmail.com>
2024-08-04 21:18:40 -0300

    Feat: Iterative Tonelli-Shanks.

--- include/tonelli_shanks.hpp
+++ include/tonelli_shanks.hpp
@@ -3,8 +3,12 @@
 
 #include <gmpxx.h>
 
+mpz_class choose_non_residue(const mpz_class p);
+
 mpz_class tonelli_shanks(const mpz_class& a, const mpz_class& p);
 
 mpz_class tonelli_shanks_recursive(const mpz_class& a, const mpz_class& b, const mpz_class& m, const mpz_class& p);
+
+mpz_class tonelli_shanks_iterative(const mpz_class& a, const mpz_class& p);
 
 #endif
