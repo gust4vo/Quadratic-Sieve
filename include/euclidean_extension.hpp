46f24524a7349673087b9f58154e1eee062410d3
IagoNathan <inate.ca98@gmail.com>
2024-08-06 15:32:55 -0300

    cipolla

--- include/euclidean_extension.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef EUCLIDEAN_EXTENSION
-#define EUCLIDEAN_EXTENSION
-
-#include <gmpxx.h>
-
-std::pair<mpz_class, mpz_class> euclidean_extension(const mpz_class& a, const mpz_class& b);
-
-#endif
