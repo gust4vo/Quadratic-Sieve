47e1a4fa204f4486dadf06e01a4fab5c5e0f28fe
Leviathan <soaresjsantos93@gmail.com>
2024-08-11 00:43:02 -0300

    Feat: printing the matriz.

--- src/quadratic_sieve.cpp
+++ src/quadratic_sieve.cpp
@@ -12,15 +12,17 @@ void quadratic_sieve(std::vector<mpz_class> &primes, mpz_class n) {
 
     if (x*x < n) x++;
 
-    mpz_class possible_smoth_size = primes.size() * 6;
+    mpz_class possible_smooth_size = primes.size() * 6;
 
-    std::vector<mpz_class> possible_smooth(possible_smoth_size.get_ui());
+    std::vector<mpz_class> possible_smooth(possible_smooth_size.get_ui());
     
-    for (ssize_t i = 0; i < possible_smoth_size; i++)
+    for (ssize_t i = 0; i < possible_smooth_size; i++)
         possible_smooth[i] = (x + i)*(x + i) % n; 
     
     std::vector<mpz_class> bases;
     bases.push_back(2);
+
+    std::cout << "\t\t\t\t";
 
     for (ssize_t i = 0; i < primes.size(); i++)
     {
@@ -32,27 +34,34 @@ void quadratic_sieve(std::vector<mpz_class> &primes, mpz_class n) {
     }
         std::cout << '\n';
 
+    std::vector<std::vector<unsigned long long>> pre_matriz(
+    possible_smooth_size.get_ui(), 
+    std::vector<unsigned long long>(bases.size(), 0));
 
     mpz_class tonelli_ans, ans;
     
-    tonelli_ans = cipolla(n, bases[0]);
+    tonelli_ans = cipolla(n, 2);
 
-    ans = tonelli_ans - (x % bases[0]);
+    ans = tonelli_ans - (x % 2);
 
-    for (ssize_t k = 0; k < possible_smoth_size; k++)
-        std::cout << possible_smooth[k] << ' '; 
+    // for (ssize_t k = 0; k < possible_smooth_size; k++)
+    //     std::cout << possible_smooth[k] << ' '; 
     
-    std::cout << '\n';
+    // std::cout << '\n';
     
-    for (ssize_t k = ans.get_ui(); k < possible_smoth_size; k += bases[0].get_ui())
-        if (possible_smooth[k] != 1) possible_smooth[k] /= bases[0];
+    for (ssize_t k = ans.get_ui(); k < possible_smooth_size; k += 2)
+        while(possible_smooth[k] % 2 == 0) 
+        {
+            possible_smooth[k] /= 2;
+            pre_matriz[k][0]++;
+        }
 
-    std::cout << "Toneli: " << tonelli_ans << " Begin: " << ans << " Prime :" << bases[0] << '\n';
+    // std::cout << "Toneli: " << tonelli_ans << " Begin: " << ans << " Prime :" << bases[0] << '\n';
 
-    for (ssize_t k = 0; k < possible_smoth_size; k++)
-        std::cout << possible_smooth[k] << ' ';
+    // for (ssize_t k = 0; k < possible_smooth_size; k++)
+    //     std::cout << possible_smooth[k] << ' ';
 
-    std::cout << '\n';
+    // std::cout << '\n';
     
     for (ssize_t i = 1; i < bases.size(); i++)
     {
@@ -62,42 +71,58 @@ void quadratic_sieve(std::vector<mpz_class> &primes, mpz_class n) {
 
         if (ans < 0) ans += bases[i];
         
-        for (ssize_t k = ans.get_ui(); k < possible_smoth_size; k += bases[i].get_ui())
+        for (ssize_t k = ans.get_ui(); k < possible_smooth_size; k += bases[i].get_ui())
         {
-            if (possible_smooth[k] != 1) possible_smooth[k] /= bases[i];
+            while(possible_smooth[k] % bases[i] == 0)
+            {
+                possible_smooth[k] /= bases[i];
+                pre_matriz[k][i]++;
+            } 
         }
 
-        std::cout << "Toneli: " << tonelli_ans << " Begin: " << ans << " Prime :" << bases[i] << '\n';
+        // std::cout << "Toneli: " << tonelli_ans << " Begin: " << ans << " Prime :" << bases[i] << '\n';
         
-        for (ssize_t k = 0; k < possible_smoth_size; k++)
-            std::cout << possible_smooth[k] << ' ';
+        // for (ssize_t k = 0; k < possible_smooth_size; k++)
+        //     std::cout << possible_smooth[k] << ' ';
 
-        std::cout << '\n';
+        // std::cout << '\n';
     
         ans = ((bases[i] - tonelli_ans) - (x % bases[i])) % bases[i];
 
         if (ans < 0) ans += bases[i];
         
-        for (ssize_t k = ans.get_ui(); k < possible_smoth_size; k += bases[i].get_ui())
+        for (ssize_t k = ans.get_ui(); k < possible_smooth_size; k += bases[i].get_ui())
         {
-            if (possible_smooth[k] != 1) possible_smooth[k] /= bases[i];
+            while(possible_smooth[k] % bases[i] == 0) 
+            {
+                possible_smooth[k] /= bases[i];
+                pre_matriz[k][i]++;
+            }
         }  
 
-        std::cout << "Toneli: " << tonelli_ans << " Begin: " << ans << " Prime :" << bases[i] << '\n';
+        // std::cout << "Toneli: " << tonelli_ans << " Begin: " << ans << " Prime :" << bases[i] << '\n';
 
-        for (ssize_t k = 0; k < possible_smoth_size; k++)
-            std::cout << possible_smooth[k] << ' ';
+        // for (ssize_t k = 0; k < possible_smooth_size; k++)
+        //     std::cout << possible_smooth[k] << ' ';
 
-        std::cout << '\n';
+        // std::cout << '\n';
         
         
     }
 
+    for (ssize_t i = 0; i < possible_smooth_size; i++)
+    {
+        std::cout << '\t' << (x + i)*(x + i) << "\t"  << (x + i)*(x + i) % n << "\t" << possible_smooth[i] << "\t\t";
 
+        for (ssize_t j = 0; j < bases.size(); j++)
+        {
+            std::cout << pre_matriz[i][j] << ' ';
+        }
 
-
-    for (ssize_t i = 0; i < possible_smoth_size; i++)
-        std::cout << (x + i)*(x + i) % n << " " << possible_smooth[i] << '\n';
+        std::cout << '\n';
+    }
+    
+        
     
     
     
