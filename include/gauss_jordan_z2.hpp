ae6b51d5738a490331c4b4cfca572864fef9c8d5
RodrigoLMF <rodrigoluizmf@gmail.com>
2024-08-12 16:09:54 -0300

    added files gauss_jordan_z2.hpp and gauss_jordan_z2.cpp

---
+++ include/gauss_jordan_z2.hpp
@@ -0,0 +1,8 @@
+#ifndef GAUSS_JORDAN_Z2_HPP
+#define GAUSS_JORDAN_Z2_HPP
+
+#include <vector>
+
+std::vector<int> gauss_jordan_z2(const std::vector<std::vector<int>>& matrix, const std::vector<int>& b);
+
+#endif
